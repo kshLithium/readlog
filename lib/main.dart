ff7eaaa17ffde418b5c88f0d1a3a8752ae855680
kshLithium <2071123@hansung.ac.kr>
2024-11-27 20:33:26 +0900

    firebase init - firebase core, auth 임포트 - ios 최소버전 13으로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:readlog/screens/splash_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized(); // 초기화
+  WidgetsFlutterBinding.ensureInitialized(); // 초기화
   await dotenv.load(); // .env 파일 로드
+  await Firebase.initializeApp(
+    // Firebase 초기화
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
